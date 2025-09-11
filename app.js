b971b6b74180e71c509cf9e5a69c6dd5f69d25f6
abelpormade <abel@pormade.com.br>
2025-09-11 16:03:21 -0300

    coexão com banco de dados

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ import { fileURLToPath } from 'url';
 const app = express();
 
 // Corrige __dirname em ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
 
 // Middleware para ler JSON
 app.use(express.json());
 
 // Middleware para servir arquivos da pasta "public"
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send("Página de início");
+  res.status(200).json("Página de início");
 });
 
 app.get('/inicio', (req, res) => {
