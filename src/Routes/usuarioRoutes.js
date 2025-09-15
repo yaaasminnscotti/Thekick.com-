5920472184b00386020b5b2f1e12b4f2087cd935
abelpormade <abel@pormade.com.br>
2025-09-15 17:27:42 -0300

    rotas

--- src/Routes/usuarioRoutes.js
+++ src/Routes/usuarioRoutes.js
@@ -3,13 +3,14 @@ import UsuarioController from '../controllers/usuarioController.js';
 
 const router = express.Router();
 
-router.get('/usuarios/filtro', UsuarioController.filtrarPorNome);
+router.get('/filtro', UsuarioController.filtrarPorNome);
 router.get('/', UsuarioController.listar);
 router.get('/:id', UsuarioController.listarPorId);
 router.post('/', UsuarioController.criar);
 router.put('/:id', UsuarioController.atualizar);
 router.delete('/:id', UsuarioController.deletar);
 router.get('/login/:id', UsuarioController.login);
+
 router.get('/deslogar/:id', UsuarioController.deslogar);
 
 export default router;
