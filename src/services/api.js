48361f15b31b30603392af7d5ec20baaadaf70e0
Mariana Beatriz Pinheiro <marianabeatrizpinheirombp@gmail.com>
2025-09-18 12:03:08 -0300

    sdssdff

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,26 @@
 import axios from 'axios';
 
-async function App(){
-    axios.get('http://localhost:3000/usuarios',).then((response)=>console.log(response)).catch((error)=>(console.log(error)));
+
+async function getUsuarios() {
+    try {
+        const resposta = await axios.get('/usuarios');
+        console.log(resposta);
+    } catch (erro) {
+        console.error(erro);
+    }
 }
-export default App;
+async function postUsuarios() {
+   try{
+    const {data} = await axios.post('/usuarios', document.querySelector(''), {//formulário de cadastro
+        headers: {
+    'Content-Type': 'application/json'
+    }
+    })
+    } catch(erro){
+        console.error(erro);
+    }
+}
+async function getUsuarios(params) {
+    
+}
+export default { getUsuarios, postUsuarios };
